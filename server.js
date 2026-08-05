3ccf6058db6a23599c18745a1a608d9d6c755b7d
Andres-Perero <andres.pereros@hotmail.com>
2026-08-05 17:00:53 -0500

    FIX Add page static

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { chromium } from 'playwright';
 import { resolveVimeosEmbed } from './resolvers/vimeos.js';
 import { resolveGoodstreamEmbed } from './resolvers/goodstream.js';
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
