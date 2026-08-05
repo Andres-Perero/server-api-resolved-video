5cfb214750ed42ff2509a03f742770de4a2f3e63
Andres-Perero <andres.pereros@hotmail.com>
2026-08-05 17:07:24 -0500

    FIX Add page static

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ import { chromium } from 'playwright';
 import { resolveVimeosEmbed } from './resolvers/vimeos.js';
 import { resolveGoodstreamEmbed } from './resolvers/goodstream.js';
 import path from 'path';
-
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
